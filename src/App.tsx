b2b4e4be44aa0902f00d19694131e258a3f2ec35
irwinekabakti <irwinekabakti@gmail.com>
2024-09-20 15:50:06 +0700

    add:(feat currency exchange)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import CurrencyExchange from "./components/CurrencyExchange";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="title px-4 py-8">
+        <h1 className="text-2xl font-bold mb-4 text-center">
+          Currency Exchange Rates
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CurrencyExchange />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
